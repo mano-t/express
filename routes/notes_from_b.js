9050652b3ec3a21c9b1a2a8b7d5f75e3d7771132
mano-t <129839373+mano-t@users.noreply.github.com>
2024-01-27 20:05:28 +0900

    Update notes_from_b.js

--- routes/notes_from_b.js
+++ routes/notes_from_b.js
@@ -5,39 +5,12 @@ require('dotenv').config();
 
 // 接続情報を設定
 const { MongoClient } = require("mongodb");
-<<<<<<< HEAD
 const uri = process.env.MONGODB_URI;
-=======
-const uri = "mongodb+srv://mano:Password@atlascluster.mhorg8t.mongodb.net/?retryWrites=true&w=majority";
->>>>>>> e1963d13cdc3b95eaad6601416ac3b69e2fdf8e9
 const client = new MongoClient(uri);
 
 // corsミドルウェアを使用
 router.use(cors());
 
-<<<<<<< HEAD
-// MongoDB クライアントをアプリケーション起動時に生成し、アプリケーション終了時にクローズ
-let database;
-(async () => {
-    await client.connect();
-    database = client.db('notes');
-})();
-
-router.get('/', async (req, res) => {
-    try {
-        // データベース、コレクションを指定
-        const notes = database.collection('notes');
-
-        // 全てのドキュメントを取得
-        const note = await notes.find({}).toArray();
-
-        res.json(note);
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Internal Server Error');
-    }
-});
-=======
 router.get('/', async (req, res) => {
 // データベース、コレクションを指定
 const database = client.db('notes');
@@ -49,6 +22,5 @@ const note = await notes.find({}).toArray();
 
 res.json(note);
 })
->>>>>>> e1963d13cdc3b95eaad6601416ac3b69e2fdf8e9
 
 module.exports = router;
